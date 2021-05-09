5ee6cc94b6363bb744e4c28d4b98c07e54350aa6
James Phillips <phillips.james@gmail.com>
2021-05-09 15:47:36 -0500

    adding provider

--- variables.tf
+++ variables.tf
@@ -7,3 +7,9 @@ variable "environment" {
   type        = string
   description = "environment tag value"
 }
+
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "aws region to use, defaults to us-east-1"
+}
