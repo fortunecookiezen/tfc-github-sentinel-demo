145b30b3046d736f8300a6404ece6a4a0b028f87
James Phillips <phillips.james@gmail.com>
2021-06-05 09:12:22 -0500

    adding private and isolated subnets

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,11 @@
 module "vpc" {
-  source         = "github.com/fortunecookiezen/tf-aws-vpc-module"
-  name           = "vpc"
-  cidr           = "10.10.0.0/20"
-  create_igw     = true
-  public_subnets = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  source           = "github.com/fortunecookiezen/tf-aws-vpc-module"
+  name             = "vpc"
+  cidr             = "10.10.0.0/20"
+  create_igw       = true
+  public_subnets   = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  private_subnets  = ["10.10.4.0/24", "10.10.5.0/24", "10.10.6.0/24", "10.10.7.0/24"]
+  isolated_subnets = ["10.10.8.0/24", "10.10.9.0/24", "10.10.10.0/24", "10.10.11.0/24"]
 
   tags = {
     Owner       = var.owner
