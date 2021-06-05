683c56a0a13c2c35d999e2deb626879d3a75080e
James Phillips <phillips.james@gmail.com>
2021-06-04 20:37:37 -0500

    updating config

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,9 @@
 module "vpc" {
-  source = "github.com/fortunecookiezen/tf-aws-vpc-module"
-  name   = "vpc"
-  cidr   = "10.10.0.0/20"
+  source         = "github.com/fortunecookiezen/tf-aws-vpc-module"
+  name           = "vpc"
+  cidr           = "10.10.0.0/20"
+  create_igw     = true
+  public_subnets = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
 
   tags = {
     Owner       = var.owner
